f04cb7e69a953e48ed2028a94a1d1a2e4aba943c
Kots Olesya <kots.olesya@gmail.com>
2016-10-19 19:31:42 +0300

    made 3 methods

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,29 @@
 require 'date'
+
 class Lesson1
   def sum(val = 0)
-    # TODO
+    val.to_s.split('').map(&:to_i) .inject { |a, e| a + e }
   end
 
   def age(birthday)
-    # TODO
+    if birthday.nil?
+      'Invalid Date Format'
+    else
+      date_now = Date.parse(Time.now.to_s)
+      day = (date_now - Date.parse(birthday)).to_i
+      str = "I live #{day / 365} years or #{day}, days or #{day * 24} hours,
+       or #{day * 24 * 60} minutes, or #{day * 24 * 60 * 60} seconds"
+      # puts str.include?('26')
+      str
+    end
   end
 
   def name
-    # TODO
+    puts "What's your name? "
+    your_name = gets
+    your_second_name = gets
+    your_father_name = gets
+    name_out = "Hello #{your_name} #{your_second_name} #{your_father_name}!"
+    name_out
   end
 end
